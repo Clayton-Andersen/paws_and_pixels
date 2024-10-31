53a932fb9cf766246eb63128cecd8e98d4fd5dde
Aloha <utah.clayton@gmail.com>
2024-10-30 23:25:26 -0600

    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="container mt-5">
       <h1>About Paws and Pixels</h1>
-      <p></p>
+      <p>Welcome to Paws and Pixels! We specialize in capturing the beauty of your pets.</p>
     </div>
   );
 };
